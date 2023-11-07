c406c1063bc4726ecc716ac5eab4705ace7348c3
GameDevPete <99825504+PeteGameDev@users.noreply.github.com>
2023-11-07 09:08:48 +0000

    Stuff

--- First Person Shooter/Assets/Scripts/FPS_InstantiateGun.cs
+++ First Person Shooter/Assets/Scripts/FPS_InstantiateGun.cs
@@ -7,12 +7,6 @@ public class FPS_InstantiateGun : MonoBehaviour
     public GameObject bullet;
     public GameObject firePoint;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
